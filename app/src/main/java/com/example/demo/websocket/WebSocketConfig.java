08799f4863a1851840902b165e274b565d31d366
spinliaomarais <spinliao@storemarais.com>
2023-07-22 12:11:59 +0800

    connect nginx

--- app/src/main/java/com/example/demo/websocket/WebSocketConfig.java
+++ app/src/main/java/com/example/demo/websocket/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/spin-websocket").setAllowedOriginPatterns("*");
     }
 
     @Override
